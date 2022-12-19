10c0abaa8eaa4758ed6ac5de74e40decdffa09da
sgraziola <simo.fdl@gmail.com>
2022-12-19 17:02:27 +0100

    Add some style (bonus)

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,8 +2,8 @@
 
 @section('content')
 
-<div class="container">
-    <div class="p-5 mb-4 bg-light rounded-3">
+<div class="container py-4">
+    <div class="jumbotron p-5 mb-4 rounded-3">
         <div class="container-fluid py-5">
             <h1 class="display-5 fw-bold">Benvenuto!</h1>
             <p class="col-md-8 fs-4">Milioni di film, serie TV e persone da scoprire.</p>
