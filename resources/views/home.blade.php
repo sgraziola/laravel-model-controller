b68393129e20b51a85431ac8b627527e1a86e5f0
sgraziola <simo.fdl@gmail.com>
2022-12-19 15:22:41 +0100

    create Model

---
+++ resources/views/home.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('content')
+
+<h1>Welcome to the Movie Site</h1>
+
+@endsection
---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,27 @@
+<footer class="py-5 bg-dark text-white">
+    <div class="container">
+        <div class="row">
+            <div class="col-4">
+                <a href="{{route('home')}}">MOVIES</a>
+
+            </div>
+
+            <div class="col-4">
+                <h3>Quick links</h3>
+                <ul>
+                    <li>home</li>
+                    <li>support</li>
+                    <li>news</li>
+                </ul>
+            </div>
+
+            <div class="col-4">
+                <h3>Support</h3>
+                <ul>
+                    <li>Contact Us</li>
+                    <li>Knowledge Base</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</footer>
